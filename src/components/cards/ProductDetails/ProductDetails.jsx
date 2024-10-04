b2489019d94261d4cb2228d9886215162bbcfc33
Lill-Kristine Konst <126065743+LillKonst@users.noreply.github.com>
2024-10-04 12:36:16 +0200

    media queries

--- src/components/cards/ProductDetails/ProductDetails.jsx
+++ src/components/cards/ProductDetails/ProductDetails.jsx
@@ -3,9 +3,9 @@ import AddToCartBtn from "../../buttons/AddToCartBtn/AddToCartBtn";
 export default function ProductDetails({ product }) {
 
     return (
-        <div className="p-4">
+        <div className=" p-1 md:p-4">
             <div className="max-w-5xl mx-auto">
-                <div className="flex flex-col md:flex-row justify-start mx-5">
+                <div className="flex flex-col md:flex-row justify-center mx-5">
                     <div className="flex-1 container md:mx-5 aspect-w-2 aspect-h-1 h-80">
                         <img
                             src={product.image.url}
